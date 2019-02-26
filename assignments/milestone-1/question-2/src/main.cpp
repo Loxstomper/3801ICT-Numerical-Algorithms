bc30102a14d0779b232a515b93d4e5cbf1f62e3e
Lox <lox.ash@hotmail.com>
2019-02-26 19:22:30 +1000

    calculate velocities and accelerations

--- assignments/milestone-1/question-2/src/main.cpp
+++ assignments/milestone-1/question-2/src/main.cpp
@@ -38,6 +38,38 @@ double secondDerivative(double v1, double v2, double h)
     return (v1 - v2) / h;
 }
 
+std::vector<double> calcVelocities(std::vector<Record> r)
+{
+    std::vector<double> velocities;
+
+    double v;
+
+    for (int i = 1; i < r.size(); i ++) 
+    {
+        v = firstDerivative(r[i - 1], r[i]);
+
+        velocities.push_back(v);
+    }
+
+    return velocities;
+}
+
+std::vector<double> calcAccelerations(std::vector<double> v, double h)
+{
+    std::vector<double> accelerations;
+
+    double a;
+
+    for (int i = 1; i < v.size(); i ++) 
+    {
+        a = secondDerivative(v[i - 1], v[i], h);
+
+        accelerations.push_back(a);
+    }
+
+    return accelerations;
+}
+
 int main(int argc, char** argv)
 {
     std::vector<Record> records = buildRecords();
@@ -47,7 +79,29 @@ int main(int argc, char** argv)
     //     std::cout << r.getTime() << " " << r.getAngle() << " " << r.getRadius() << std::endl;
     // }
 
-    std::cout << firstDerivative(records[0], records[1]) << std::endl;
+    // std::cout << firstDerivative(records[0], records[1]) << std::endl;
+
+    double v1 = firstDerivative(records[0], records[1]);
+    double v2 = firstDerivative(records[1], records[2]);
+
+    double a1 = secondDerivative(v1, v2, 2.0);
+
+    std::vector<double> velocities = calcVelocities(records);
+    std::vector<double> accelerations = calcAccelerations(velocities, 2.0);
+
+    std::cout << "V" << std::endl;
+
+    for (double v: velocities)
+    {
+        std::cout << v << std::endl;
+    }
+
+    std::cout << "A" << std::endl;
+
+    for (double a: accelerations)
+    {
+        std::cout << a << std::endl;
+    }
 
     exit(0);
 }
