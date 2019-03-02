ce9df06f83ebb81df7343ab20e96be29c76ff34a
loxstomper <lox.ash@hotmail.com>
2019-03-02 13:45:52 +1000

    done

--- weekly-problems/computational-errors/2/2.c
+++ weekly-problems/computational-errors/2/2.c
@@ -11,13 +11,13 @@ int factorial(int n)
 
 double calculate(double x, int terms)
 {
-    double result = 1;
+    // double result = 1;
+    double result = 0;
 
     for (int i = 0; i < terms; i ++)
     {
         result += (pow(x, i)) / factorial(i);
     }
-
 
     return result;
 }
@@ -25,15 +25,25 @@ double calculate(double x, int terms)
 int main(int argc, char** argv) 
 {
     double x = 0.5;
-    double result;
+    double previous = 999;
+    double current;
     int terms = 1;
 
-    while (terms < 5)
-    {
-        result = calculate(x, terms);
-        printf("%f\n", result);
-        terms ++;    
+    // erro 0.05% for 3 sig digits
+    double Es = 0.05;
 
+    // approx relative error
+    double are = 99;
+
+    while (are > Es)
+    {
+        current = calculate(x, terms);
+        are = abs((current - previous) / current) * 100;
+
+        printf("%d %f %f\n", terms, current, are);
+
+        previous = current;
+        terms ++;    
     }
 
 
