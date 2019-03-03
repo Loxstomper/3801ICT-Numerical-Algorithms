54c49f899e610a63c0cd846185c9b1160e2a4529
Lox <lox.ash@hotmail.com>
2019-03-03 21:09:26 +1000

    WIP: question 9 - not accurate enough

---
+++ weekly-problems/computational-errors/9/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int factorial(int n) 
+{
+    if (n < 1) 
+    {
+        return 1;
+    }
+
+    return n * factorial(n - 1);
+}
+
+double calc(double n, double Es) 
+{
+    double result = 1;
+    double error = 999;
+    double previous = 999;
+    int neg = 1;
+
+    for (int i = 2; error > Es ; i += 2) 
+    {
+        if (!neg) 
+        {
+            result += (pow(n, i) / factorial(i));
+            neg = 1;
+        }
+        else 
+        {
+            result -= (pow(n, i) / factorial(i));
+            neg = 0;
+        }
+
+        error = abs((result - previous) / result) * 100;
+        previous = result;
+
+        printf("%f\n", result);
+    }
+
+    return result;
+}
+
+
+int main(int argc, char** argv)
+{
+    int n_sig_figures = 8;
+
+    double Es = (0.5 * pow(10, (2 - n_sig_figures))) * 100;
+    double n = 0.3 * M_PI;
+
+    printf("%f \n", calc(n, Es));
+
+    return 0;
+}
