e2833216f3e35a6894b9c9669fcae1022e0be185
loxstomper <lox.ash@hotmail.com>
2019-03-05 19:56:12 +1000

    Started taylor series

---
+++ weekly-problems/taylor-series/1/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+    // zero thru fourth-order taylor series expansions to approximate the function
+    // f(x) = -0.1x^4 - 0.15x^2 - 0.25x + 1.2 from xi = 0, h = 1
+    return 0;
+}
