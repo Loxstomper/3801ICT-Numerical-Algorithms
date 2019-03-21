19f4e824509f734e2b176bd192eb1ddb96bc6217
Lox <lox.ash@hotmail.com>
2019-03-22 00:12:45 +1000

    ode question 1

--- weekly-problems/ordinary-differential-equations/1/main.cpp
+++ weekly-problems/ordinary-differential-equations/1/main.cpp
@@ -8,17 +8,21 @@ double df(double x)
 
 int main(int argc, char** argv)
 {
-    double x = 0, y = 1;
+    double stepSize = 0.5;
+    double x = 0, yPrev = 1, y;
 
-    std::cout << x << " " << y << std::endl;
+    std::cout << x << " " << yPrev << "\n" << std::endl;
 
     for(int i = 0; i < 8; i++)
     {
-        y += df(x) * 0.5;
-        x += 0.5;
+        y = yPrev + df(x) * stepSize * 0.5;
+        yPrev = y;
+        x += stepSize;
 
-        std::cout << x << " " << y << std::endl;
-    }
+        std::cout << x << "\t" << y << std::endl;
+    } 
+
+    std::cout << "Done: " << y << std::endl;
     
     exit(0);
 }
